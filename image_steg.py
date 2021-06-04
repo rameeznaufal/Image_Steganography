a2f7ca07bbe6fb17fb55f5be4cde80e76983f673
rameeznaufal <rameeznaufal12@gmail.com>
2021-06-04 20:44:31 +0530

    "2nd commit"

--- image_steg.py
+++ image_steg.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import types
+from pyfiglet import Figlet
 
 def messageToBinary(message):
     if type(message) == str:
@@ -11,3 +12,23 @@ def messageToBinary(message):
         return format(message, "08b")
     else:
         raise TypeError("Input type not supported")
+
+def Steganography(): 
+    print()
+    f=Figlet(font='alligator2',justify='center',width=150)
+    print(f.renderText('I m a g e'))
+    print(f.renderText('Steganography'))
+    a = input(" \n 1. Encode the data \n 2. Decode the data \n Your input is: ")
+    userinput = int(a)
+    if (userinput == 1):
+      print("\nEncoding....")
+      #encode_text() 
+    elif (userinput == 2):
+      print("\nDecoding....") 
+      print("Decoded message is "                ) 
+                                  #+  decode_text() 
+    else: 
+      raise Exception("Enter correct input") 
+
+if __name__ == "__main__":
+    Steganography()        
